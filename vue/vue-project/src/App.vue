28a8ac99057c95d7ca2918d5ca2e1266ec8afc73
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-04-01 22:30:12 +0200

    vue

---
+++ vue/vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Header from "./components/Header.vue";
+
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
+</style>
