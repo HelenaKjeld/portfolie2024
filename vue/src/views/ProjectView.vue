08feb4f46f75358afb558c27554e6d6e1b17a1fd
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2024-04-07 22:39:02 +0200

    routter link and img

---
+++ vue/src/views/ProjectView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="project">
+      <h1>This is a project page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .project {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
